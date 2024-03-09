0561de6c71393cb15dbdd56332820cb35767b56d
Caballego <victor.caballero@ucb.edu.com>
2024-03-08 20:12:27 -0400

    Práctica 1-Parte 2

--- PRACTICA_1/PRACTICA_1.ino
+++ PRACTICA_1/PRACTICA_1.ino
@@ -1,12 +1,15 @@
 #define LED1 3
 #define LED2 4
 #define LED3 5
+#define PULSADOR 7
+bool secuencia=true;
 
 void setup() 
 {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
+  pinMode(PULSADOR, INPUT_PULLUP);
   digitalWrite(LED1, LOW);
   digitalWrite(LED2, LOW);
   digitalWrite(LED3, LOW);
@@ -14,16 +17,38 @@ void setup()
 
 void loop() 
 {
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, LOW);
-  delay(200);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, LOW);
-  delay(200);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, HIGH);
-  delay(200);
+  if (digitalRead(PULSADOR) == LOW)
+  {
+    secuencia=!secuencia;
+  }
+  if (secuencia==true)
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    delay(200);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    delay(200);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+    delay(200);
+  }
+  else
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    delay(200);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+    delay(200);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    delay(200);
+  }
 }
