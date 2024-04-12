a8a560bab46c935a3249a63dafd41571d781d934
Caballego <victor.caballero@ucb.edu.com>
2024-04-12 19:26:09 -0400

    Práctica 4

---
+++ PRACTICA_4/PRACTICA_4.ino
@@ -0,0 +1,79 @@
+#define PULSADOR1 2
+#define PULSADOR2 3
+#define LED 4
+volatile int contador=0;
+volatile int frecuencia=0;
+int delay_frecuencia=0;
+int contador2=0;
+const int tiempo_limite = 40;
+long tiempo1 = 0;
+long tiempo2 = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("INICIADO");
+  Serial.print("Número de interrupciones: ");
+  Serial.println(contador);
+  pinMode(PULSADOR1, INPUT_PULLUP);
+  pinMode(PULSADOR2, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  attachInterrupt(digitalPinToInterrupt(PULSADOR1), actualizar_contador, RISING);
+  attachInterrupt(digitalPinToInterrupt(PULSADOR2), reiniciar_contador, RISING);
+}
+
+void loop() 
+{
+  if (frecuencia==0)
+  {
+    delay_frecuencia=500;
+  }
+  else if (frecuencia==1)
+  {
+    delay_frecuencia=1000;
+  }
+  else
+  {
+    delay_frecuencia=1500;
+  }
+  digitalWrite(LED, HIGH);
+  delay(delay_frecuencia);
+  digitalWrite(LED, LOW);
+  delay(delay_frecuencia);
+}
+
+void actualizar_contador()
+{
+  tiempo1=millis();
+  if (tiempo1-tiempo2 > tiempo_limite)
+  {
+    contador++;
+    Serial.print("Número de interrupciones: ");
+    Serial.println(contador);
+    tiempo2=millis();
+  }
+  if (tiempo2-tiempo1>3000)
+  {
+    Serial.println("Contador reiniciado");
+  }
+}
+
+void reiniciar_contador()
+{
+  if (millis()-tiempo2 > tiempo_limite)
+  {
+    contador=0;
+    Serial.println("Contador reiniciado-Frecuencia del led actualizada");
+    Serial.println("Número de interrupciones: 0");
+    if (frecuencia!=2)
+    {
+      frecuencia++;
+    }
+    else
+    {
+      frecuencia=0;
+    }
+  }
+}
